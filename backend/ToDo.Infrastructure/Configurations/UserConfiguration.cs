580ae1f4e95b6e80a0d19c0447bc74c8e0e46cf1
Maksym <monctrikua@gmail.com>
2026-06-04 00:14:02 +0300

    feat(infra): configure user-category relationship

--- backend/ToDo.Infrastructure/Configurations/UserConfiguration.cs
+++ backend/ToDo.Infrastructure/Configurations/UserConfiguration.cs
@@ -20,5 +20,10 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
             .WithOne(x => x.User)
             .HasForeignKey(x => x.UserId)
             .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.HasMany(x => x.Categories)
+            .WithOne(x => x.User)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
