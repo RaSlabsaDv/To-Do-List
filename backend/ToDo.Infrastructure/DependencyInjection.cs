4fd045d9a0688e7e5ed566b9ede150160131f009
Maksym <monctrikua@gmail.com>
2026-06-04 23:42:29 +0300

    feat: setup db via docker, db connection and DI registration

---
+++ backend/ToDo.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure
+    (
+        this IServiceCollection services, 
+        IConfiguration configuration
+    )
+    {
+        services.AddDbContext<ToDoContext>(opt =>
+            opt.UseNpgsql(configuration.GetConnectionString("Default")));
+
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUserTaskRepository, UserTaskRepository>();
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+        return services;
+    }
+}
