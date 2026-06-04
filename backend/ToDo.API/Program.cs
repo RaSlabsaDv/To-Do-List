551f11f4d2d76b0c5b2950f9d8923aac8a3a766b
Maksym <monctrikua@gmail.com>
2026-06-04 19:54:44 +0300

    feat: add NotFoundException and global exception handler

--- backend/ToDo.API/Program.cs
+++ backend/ToDo.API/Program.cs
@@ -1,7 +1,25 @@
+using Microsoft.AspNetCore.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler(err => err.Run(async context =>
+{
+    var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+    var (status, message) = exception switch
+    {
+        NotFoundException => (StatusCodes.Status404NotFound, exception.Message),
+        _ => (StatusCodes.Status500InternalServerError, "Internal server error")
+    };
+
+    context.Response.StatusCode = status;
+    await context.Response.WriteAsJsonAsync(new { error = message });
+}));
 
 app.Run();
