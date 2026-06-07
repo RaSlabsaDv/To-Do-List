d402e67fb81d034d2567abc55139c316e3849341
Maksym <monctrikua@gmail.com>
2026-06-07 16:02:18 +0300

    feat: add preview page

---
+++ frontend/src/app/features/preview/preview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-preview',
+  imports: [RouterLink],
+  templateUrl: './preview.html',
+  
+})
+export class Preview {
+}
